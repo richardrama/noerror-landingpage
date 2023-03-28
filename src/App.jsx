d7b02ac29f076a7fedabb687847d30b4e653d44f
richardrama <113200103@student.upnyk.ac.id>
2023-03-28 21:32:31 +0700

    poke

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { NavBar, Home, About, Service, Projects, Testimonials, Contact, Footer } from './component'
+import About from './component/About'
+import Contact from './component/Contact'
+import Footer from './component/Footer'
+import Home from './component/Home'
+import NavBar from './component/NavBar'
+import Projects from './component/Projects'
+import Service from './component/Service'
+import Testimonials from './component/Testimonials'
 
 const App = () => {
   return (
