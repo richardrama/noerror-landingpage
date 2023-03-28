9c7ab2ad059bbfb8a7168bb518123e5b7e22a634
richardrama <113200103@student.upnyk.ac.id>
2023-03-28 22:27:09 +0700

    pke

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
         <div className={`bg-darkGreen w-full flex flex-col items-center`}>
             {/* Desktop */}
             <div className={` flex justify-between items-center py-[20px] px-[15px] sm:py-[40px] sm:px-[35px] w-full max-w-[1440px] `}>
-                <img src={logoText} alt="Logo" className='h-[36px] sm:h-auto' />
+                <img src="../assets/logo-text.png" alt="Logo" className='h-[36px] sm:h-auto' />
                 <ul className='hidden sm:flex items-center'>
                     <li className=''><img src="/noerror-landingpage/src/assets/discord.svg" alt="discord" /></li>
                     <li className='ml-[35px]'><img src='/noerror-landingpage/src/assets/github.svg' alt="github" /></li>
