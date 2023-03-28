0c1d96b56e6afe1e682e9764caf3d61fbad24ccd
richardrama <113200103@student.upnyk.ac.id>
2023-03-28 22:24:53 +0700

    ple

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { logoText } from '../assets'
 
 const NavBar = () => {
     return (
         <div className={`bg-darkGreen w-full flex flex-col items-center`}>
             {/* Desktop */}
             <div className={` flex justify-between items-center py-[20px] px-[15px] sm:py-[40px] sm:px-[35px] w-full max-w-[1440px] `}>
-                <img src="/noerror-landingpage/src/assets/logo-text.png" alt="Logo" className='h-[36px] sm:h-auto' />
+                <img src={logoText} alt="Logo" className='h-[36px] sm:h-auto' />
                 <ul className='hidden sm:flex items-center'>
                     <li className=''><img src="/noerror-landingpage/src/assets/discord.svg" alt="discord" /></li>
                     <li className='ml-[35px]'><img src='/noerror-landingpage/src/assets/github.svg' alt="github" /></li>
